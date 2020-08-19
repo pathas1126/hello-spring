62a991846f39567d17235010b446a9fc39c3997c
baripark <baripark@dailybutton.co.kr>
2020-08-19 18:50:54 +0900

    Apply JPA

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional // JPA는 모든 데이터 변경이 트랜잭션 내부에서 실행돼야 함
 public class MemberService { // 서비스 내부 메서드는 비즈니스와 관련된 용어를 사용하는 것이 좋음
 
   private final MemberRepository memberRepository;
